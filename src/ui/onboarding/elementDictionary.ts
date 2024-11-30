08639afcb45eb207eb00ea9e7b1116edb1bbb84e
black-RAM <pplanes311@gmail.com>
2024-11-30 20:30:28 +0300

    fixed bug of sidebar disapearing when clicked on wider screen

--- src/ui/onboarding/elementDictionary.ts
+++ src/ui/onboarding/elementDictionary.ts
@@ -1,44 +1,44 @@
-import elementDictionaryInterface from "../../interfaces/elementDictionaryInterface"
+import elementDictionaryInterface from "../../interfaces/elementDictionaryInterface";
 
 // factory function pattern for more dynamic object creation
 const createElementDictionary: () => elementDictionaryInterface = () => {
   const stepElement = (get: () => HTMLElement | null, stagehand = false, changer = false) => {
-    const isVisible = () => !get()?.classList.contains("d-none")
-    return {get, stagehand, changer, isVisible}
-  }
+    const isVisible = () => !get()?.classList.contains("d-none");
+    return {get, stagehand, changer, isVisible};
+  };
 
-  const banner = stepElement(() => document.getElementsByTagName("h2")[0])
+  const banner = stepElement(() => document.getElementsByTagName("h2")[0]);
   
-  const toDoArticle1 = stepElement(() => document.getElementsByTagName("article")[0], false, true)
-  const toDoArticle2 = stepElement(() => document.getElementsByTagName("article")[1], false, true)
-  const toDoArticle3 = stepElement(() => document.getElementsByTagName("article")[2], false, true)
-  const toDoArticle4 = stepElement(() => document.getElementsByTagName("article")[3], false, true)
-  const toDoArticle5 = stepElement(() => document.getElementsByTagName("article")[4], false, true)
-  const toDoArticle6 = stepElement(() => document.getElementsByTagName("article")[5], false, true)
+  const toDoArticle1 = stepElement(() => document.getElementsByTagName("article")[0], false, true);
+  const toDoArticle2 = stepElement(() => document.getElementsByTagName("article")[1], false, true);
+  const toDoArticle3 = stepElement(() => document.getElementsByTagName("article")[2], false, true);
+  const toDoArticle4 = stepElement(() => document.getElementsByTagName("article")[3], false, true);
+  const toDoArticle5 = stepElement(() => document.getElementsByTagName("article")[4], false, true);
+  const toDoArticle6 = stepElement(() => document.getElementsByTagName("article")[5], false, true);
 
-  const menuIcon = stepElement(() => document.querySelector<HTMLElement>("i.bi-list"), false, true)
-  const navBar = stepElement(() => document.getElementsByTagName("nav")[0])
-  const tourButton = stepElement(() => document.querySelector<HTMLButtonElement>("button[title='create tour']"))
-  const category2 = stepElement(() => document.querySelectorAll<HTMLLIElement>("#category-list > li")[1])
-  const addProjectIcon = stepElement(() => document.getElementById("add-project-icon"))
-  const projectHeader = stepElement(() => document.getElementById("project-header"))
-  const projectLinkLI = stepElement(() => document.getElementById("project-list"))
+  const menuIcon = stepElement(() => document.getElementById("menu-icon"), false, true);
+  const sidebar = stepElement(() => document.getElementsByTagName("nav")[0]);
+  const tourButton = stepElement(() => document.querySelector<HTMLButtonElement>("button[title='create tour']"));
+  const category2 = stepElement(() => document.querySelectorAll<HTMLLIElement>("#category-list > li")[1]);
+  const addProjectIcon = stepElement(() => document.getElementById("add-project-icon"));
+  const projectHeader = stepElement(() => document.getElementById("project-header"));
+  const projectLinkLI = stepElement(() => document.getElementById("project-list"));
   const projectLink = stepElement(() => {
-    const link = document.querySelectorAll<HTMLElement>("#project-list > li > p")[0]
+    const link = document.querySelectorAll<HTMLElement>("#project-list > li > p")[0];
 
-    if(menuIcon.isVisible() && navBar.isVisible()) {
-      navBar.get()?.addEventListener("click", () => {
-        navBar.get()?.classList.add("d-none")
-      })
-    }
+    sidebar.get()?.addEventListener("click", () => {
+      if(menuIcon.isVisible() && sidebar.isVisible()) {
+        sidebar.get()?.classList.add("d-none");
+      };
+    });
     
-    return link
-  }, true)
+    return link;
+  }, true);
 
-  const addToDoIcon = stepElement(() => document.querySelector<HTMLElement>("i.bi-journal-plus"))
-  const gameIcon = stepElement(() => document.getElementById("game-icon"), true)
+  const addToDoIcon = stepElement(() => document.querySelector<HTMLElement>("i.bi-journal-plus"));
+  const gameIcon = stepElement(() => document.getElementById("game-icon"), true);
   
-  const gameImg = stepElement(() => document.getElementsByTagName("img")[0])
+  const gameImg = stepElement(() => document.getElementsByTagName("img")[0]);
 
   return {
     banner,
@@ -49,7 +49,7 @@ const createElementDictionary: () => elementDictionaryInterface = () => {
     toDoArticle5,
     toDoArticle6,
     menuIcon,
-    navBar,
+    sidebar,
     tourButton,
     category2,
     projectHeader,
@@ -59,10 +59,10 @@ const createElementDictionary: () => elementDictionaryInterface = () => {
     addToDoIcon,
     gameIcon,
     gameImg
-  }
-}
+  };
+};
 
 // singleton
-const elementDictionary = createElementDictionary()
+const elementDictionary = createElementDictionary();
 
-export default elementDictionary
+export default elementDictionary;
