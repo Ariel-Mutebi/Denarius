db4c6c6f150c3744265ec6e4817efe564548efb6
black-RAM <pplanes311@gmail.com>
2024-12-12 17:09:47 +0300

    added to interaction overview: project creation sequence diagram

--- src/storage/loadProjects.ts
+++ src/storage/loadProjects.ts
@@ -2,23 +2,14 @@ import Project from "../classes/Project";
 import ToDo from "../classes/ToDo";
 
 function loadProjects(): Project[] {
-  const projectsJSON = localStorage.getItem("projects");
-  const projectsData: Project[] = projectsJSON ? JSON.parse(projectsJSON) : [];
-  const loadedProjects: Project[] = [];
-
-  for (const project of projectsData) {
-    const loadedProject = new Project(project.name, undefined, project.icon);
-
-    for (const todo of project.toDos) {
-      loadedProject.addToDo(
-        new ToDo(todo.title, todo.description, new Date(todo.dueDate), todo.priority)
-      );
-    };
-
-    loadedProjects.push(loadedProject);
-  };
-
-  return loadedProjects;
+  const stringifiedJSON = localStorage.getItem("projects");
+  const parsedJSON: Project[] = stringifiedJSON ? JSON.parse(stringifiedJSON) : [];
+  return parsedJSON.map(p => 
+    new Project(
+      p.name,
+      p.toDos.map(t => new ToDo(t.title, t.description, new Date(t.dueDate), t.priority)),
+      p.icon)
+    );
 };
 
 export default loadProjects;
