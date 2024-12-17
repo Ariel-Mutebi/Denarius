d255539369c0e64ad71b824f6d3533ad4d8770b7
black-RAM <pplanes311@gmail.com>
2024-12-17 21:33:19 +0300

    more rigorous tests for IDed class.
    Note to self:
    Ariel, you've been an absolute machine today! Great job! I'm proud of you!

--- src/classes/IDed.test.ts
+++ src/classes/IDed.test.ts
@@ -1,6 +1,22 @@
-import IDed from "./IDed"
+import { v4 } from "uuid";
+import uuidRegex from "../regex/uuidRegex";
+import IDed from "./IDed";
 
-test("IDed class constructor", () => {
-  const i = new IDed();
-  expect(typeof i.ID === "string" && i.ID.length === 36).toBe(true);
-})
+describe("IDed class", () => {
+  test("ID property is UUID.", () => {
+    const i = new IDed();
+    expect(uuidRegex.test(i.ID)).toBe(true);
+  });
+
+  test("accept valid UUID as input and assigns it to ID property.", () => {
+    const ID = v4();
+    const i = new IDed(ID);
+    expect(i.ID).toBe(ID);
+  });
+
+  test("does not accept NIL UUID, set its own UUID.", () => {
+    const nilUUID = "00000000-0000-0000-0000-000000000000";
+    const i = new IDed(nilUUID);
+    expect(i.ID).not.toBe(nilUUID);
+  });
+});
