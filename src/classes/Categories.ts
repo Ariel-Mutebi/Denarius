ace934085f2eb3e127bd65fe69d224de2a031a9f
black-RAM <pplanes311@gmail.com>
2024-12-18 21:16:31 +0300

    removed Group genders from class diagram

--- src/classes/Categories.ts
+++ src/classes/Categories.ts
@@ -1,8 +1,7 @@
 import CategoryInterface from "../interfaces/CategoryInterface";
 import GroupOfGroups from "./GroupOfGroups";
 
-class Categories extends GroupOfGroups<CategoryInterface> {}
+class Categories extends GroupOfGroups<CategoryInterface> {};
+const CategoriesInstance = new Categories();
 
-const CategoriesInstance = new Categories()
-
-export default CategoriesInstance
+export default CategoriesInstance;
